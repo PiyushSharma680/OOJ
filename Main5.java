8853e13a660362eb036c1cf9a1445c12975cd4b3
PiyushSharma680 <piyushsharma.cs23@bmsce.ac.in>
2024-10-27 18:49:52 +0530

    Add files via upload

---
+++ Main5.java
@@ -0,0 +1,56 @@
+abstract class Shape {
+    protected int dimension1; 
+    protected int dimension2; 
+    
+    public Shape(int dimension1, int dimension2) {
+        this.dimension1 = dimension1;
+        this.dimension2 = dimension2;
+    }
+    
+    public abstract void printArea();
+}
+
+class Rectangle extends Shape {
+    public Rectangle(int length, int width) {
+        super(length, width);
+    }
+    @Override
+    public void printArea() {
+        int area = dimension1 * dimension2;
+        System.out.println("Area of Rectangle: " + area);
+    }
+}
+
+class Triangle extends Shape {
+    public Triangle(int base, int height) {
+        super(base, height);
+    }
+    @Override
+    public void printArea() {
+        double area = 0.5 * dimension1 * dimension2; 
+        System.out.println("Area of Triangle: " + area);
+    }
+}
+
+class Circle extends Shape {
+    public Circle(int radius) {
+        super(radius, 0); 
+    }
+    @Override
+    public void printArea() {
+        double area = Math.PI * dimension1 * dimension1; 
+        System.out.println("Area of Circle: " + area);
+    }
+}
+
+public class Main5 {
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle(5, 10);
+        Shape triangle = new Triangle(6, 8);
+        Shape circle = new Circle(4);
+
+        rectangle.printArea();
+        triangle.printArea(); 
+        circle.printArea(); 
+    }
+}
